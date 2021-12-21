020b8ca29cca7fa1fa070cab7b43f84b3cab3a97
johnsiefert <johnsief12@gmail.com>
2021-12-21 17:25:56 -0500

    added api endpoints

---
+++ config/connection.js
@@ -0,0 +1,19 @@
+const Sequelize = require('sequelize');
+
+require('dotenv').config();
+
+// create connection to our db
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
+  host: 'localhost',
+  host: 'localhost',
+  dialect: 'mysql',
+  port: 3306
+});
+
+
+//All we're doing here is importing the base Sequelize class and using it to create a new connection to the database.
+//The new Sequelize() function accepts the database name, MySQL username, and MySQL password
+//parameters, then we also pass configuration settings. Once we're done, we simply export the connection.
+//import the sequelize constructor from the library
+
+module.exports = sequelize;
